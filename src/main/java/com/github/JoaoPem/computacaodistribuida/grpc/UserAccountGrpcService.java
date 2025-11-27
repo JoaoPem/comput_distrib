e51cd37ade5813badd6bc8113a78e91e634d8a9b
João Pedro <devjoaomonteiro@gmail.com>
2025-11-27 12:49:22 -0300

    gRPC User

--- src/main/java/com/github/JoaoPem/computacaodistribuida/grpc/UserAccountGrpcService.java
+++ src/main/java/com/github/JoaoPem/computacaodistribuida/grpc/UserAccountGrpcService.java
@@ -1,7 +1,9 @@
 package com.github.JoaoPem.computacaodistribuida.grpc;
 
+import com.github.JoaoPem.computacaodistribuida.services.PlaylistService;
 import com.github.JoaoPem.computacaodistribuida.services.UserAccountService;
 import io.grpc.stub.StreamObserver;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
 
@@ -12,6 +14,7 @@ import java.util.List;
 public class UserAccountGrpcService extends UserAccountServiceGrpc.UserAccountServiceImplBase {
 
     private final UserAccountService userAccountService;
+    private final PlaylistService playlistService;
 
     @Override
     public void listAllUserAccounts(
@@ -29,6 +32,40 @@ public class UserAccountGrpcService extends UserAccountServiceGrpc.UserAccountSe
                         .setName(user.getName())
                         .build()
         ));
+
+        responseObserver.onNext(responseBuilder.build());
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    @Transactional
+    public void listUserPlaylists(
+            UserIdRequest request,
+            StreamObserver<PlaylistListResponse> responseObserver) {
+
+        List<com.github.JoaoPem.computacaodistribuida.models.Playlist> playlists =
+                playlistService.listPlaylistsByUserId(request.getUserId());
+
+        if (playlists == null) playlists = List.of();
+
+        PlaylistListResponse.Builder responseBuilder = PlaylistListResponse.newBuilder();
+
+        playlists.forEach(playlist -> {
+            List<String> musicNames = playlist.getMusicList() != null
+                    ? playlist.getMusicList().stream().map(m -> m.getName()).toList()
+                    : List.of();
+
+            responseBuilder.addPlaylists(
+                    com.github.JoaoPem.computacaodistribuida.grpc.Playlist.newBuilder()
+                            .setId(playlist.getId())
+                            .setName(playlist.getName())
+                            .setUserAccountName(
+                                    playlist.getUser() != null ? playlist.getUser().getName() : ""
+                            )
+                            .addAllMusicNameList(musicNames)
+                            .build()
+            );
+        });
 
         responseObserver.onNext(responseBuilder.build());
         responseObserver.onCompleted();
